5b6114c9dd8bbbfb611ec8378a8e2d467ea29ee2
AlisonZ <alisonkzerbe@gmail.com>
2017-05-02 17:13:38 -0700

    sets up routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'homepages#index'
+
+  resources :recipes, only: [:index, :show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
